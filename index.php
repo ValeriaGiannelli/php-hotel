2bae440dfb975b44d4c4995b549ab87ce553dbe1
Valeria Giannelli <vg.giannellivaleria@gmail.com>
2024-07-24 14:41:09 +0200

    feat: create table with the informations

---
+++ index.php
@@ -0,0 +1,89 @@
+<?php
+
+// array di hotel
+    $hotels = [
+
+        [
+            'name' => 'Hotel Belvedere',
+            'description' => 'Hotel Belvedere Descrizione',
+            'parking' => true,
+            'vote' => 4,
+            'distance_to_center' => 10.4
+        ],
+        [
+            'name' => 'Hotel Futuro',
+            'description' => 'Hotel Futuro Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 2
+        ],
+        [
+            'name' => 'Hotel Rivamare',
+            'description' => 'Hotel Rivamare Descrizione',
+            'parking' => false,
+            'vote' => 1,
+            'distance_to_center' => 1
+        ],
+        [
+            'name' => 'Hotel Bellavista',
+            'description' => 'Hotel Bellavista Descrizione',
+            'parking' => false,
+            'vote' => 5,
+            'distance_to_center' => 5.5
+        ],
+        [
+            'name' => 'Hotel Milano',
+            'description' => 'Hotel Milano Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 50
+        ],
+
+    ];
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hotel</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body>
+<table class="table">
+
+  <thead>
+  
+    <tr>
+        <th scope="col">#</th>
+        <th scope="col">Nome</th>
+        <th scope="col">Descrizione</th>
+        <th scope="col">Presenza del parcheggio</th>
+        <th scope="col">Voti</th>
+        <th scope="col">Distanza dal centro</th>
+    </tr>
+  
+  </thead>
+
+  <tbody>
+    <?php foreach($hotels as $hotel): ?>
+    <tr>
+      <th scope="row">1</th>
+      <td><?php echo $hotel['name']?></td>
+      <td><?php echo $hotel['description']?></td>
+      <td><?php echo $hotel['parking']?></td>
+      <td><?php echo $hotel['vote']?></td>
+      <td><?php echo $hotel['distance_to_center']?></td>
+    </tr>
+    <?php endforeach; ?>
+
+  </tbody>
+</table>
+
+
+    
+</body>
+</html>
